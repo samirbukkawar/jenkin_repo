68475f7b91c0e2c8d9bbd8476cc8f9a8e36ba219
samirbukkawar <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 12:07:55 +0530

    Change commit

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -2,8 +2,12 @@ package org.ravi.helloworld;
 
 public class Hello {
 
+public int i;
+public int j;
 	
 	public String displayHelloMessage(String message){
+
+String sam;
 		 
 		return "hi " + message;
 	}
